8ddf6094542529a69dfa3ff23639d7aabc84a567
assulkhaniproject <assulkhaniproject@gmail.com>
2022-04-09 16:11:58 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      color: {
+        primary: '#14b8a6',
+      },
+      screens:{
+        '2xl' : '1320px'
+      } 
+    },
+  },
+  plugins: [],
+}
